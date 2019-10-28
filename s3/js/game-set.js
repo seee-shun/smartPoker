8f84da4f10b445d3ee73fb59f8bff8dfe2e6847b
RyoJosaki <46699768+RyoJosaki@users.noreply.github.com>
2019-10-28 23:53:40 +0900

    S3add

---
+++ s3/js/game-set.js
@@ -0,0 +1,22 @@
+import page2 from './page2.vue'
+var vm = new Vue({
+    el: "#app", // Vue.jsを使うタグのIDを指定
+    data() {
+        return {
+            player: 4,
+            maxRound: 4,
+            chip: 300,
+            minBet: 10,
+            players: [],
+        };
+    },
+    methods: {
+        submit: function () {
+            this.$store.state.player = Number(this.player);
+            this.$store.state.maxRound = Number(this.maxRound);
+            this.$store.state.chip = Number(this.chip);
+            this.$store.state.minBet = Number(this.minBet);
+            this.$store.commit("navigator/push", page2);
+        }
+    }
+});
